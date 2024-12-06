a24ac663af0165394e895b3ab9695270fb374d5e
SnepCnep <93768063+SnepCnep@users.noreply.github.com>
2024-12-06 13:28:49 +0100

    feat: initialize sc-easyBan and sc-lib with core configurations and functions

---
+++ sc-easyBan/config.lua
@@ -0,0 +1,4 @@
+Config = {}
+
+Config.resourceName = GetCurrentResourceName()
+Config.ServerName = "Your Server Name"
---
+++ sc-easyBan/src/client/main.lua
@@ -0,0 +1,14 @@
+local function exports(exportName, exportFunc)
+    assert(type(exportName) == "string", "exportName must be a string")
+    assert(type(exportFunc) == "function", "exportFunc must be a function")
+
+    AddEventHandler(('__cfx_export_%s_%s'):format(Config.resourceName, exportName), function(setCB)
+        setCB(exportFunc)
+    end)
+end
+
+exports("banPlayer", function(reason)
+    local resource = (GetInvokingResource() or "unkown")
+
+    TriggerServerEvent("sc-easyBan:banPlayer", reason, resource)
+end)
